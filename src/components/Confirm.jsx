546d16e63e7a111481c31bc052a7ce194e2baeca
dominionolonilebi <dominionolufunke27@gmail.com>
2024-07-09 14:44:19 +0100

    updatess

--- src/components/Confirm.jsx
+++ src/components/Confirm.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BiSolidMessageRoundedError } from 'react-icons/bi'
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom'
+import { getCartTotal } from '../redux/CartSlice';
 
 const Confirm = () => {
+
+        const dispatch = useDispatch();
+        const {
+          data: cartProds,
+          totalAmounts,
+          deliveryCharge,
+        } = useSelector((state) => state.cart);
+      
+        useEffect(() => {
+          dispatch(getCartTotal());
+        }, [useSelector((state) => state.cart)]);
+
+
   return (
     <section fluid className='py-5 confirm'>
         <div className='container py-3'>
@@ -14,7 +29,7 @@ const Confirm = () => {
                         <p className='acct-details'>Account Name: Everything Gadgets</p>
                         <p className='acct-details'>Bank Name: Paystack</p>
                         <p>Account Number: 10167321962</p>
-                        <p>Amount: </p>
+                        <p>Amount: ${totalAmounts + deliveryCharge}</p>
                         <button className='error-text mt-2 py-2 mb-4'><BiSolidMessageRoundedError className='error-icon'/> Ensure you transfer the exact amount stated above!</button>
                         <button className='confirm-btn py-2 px-4 border-0 mb-2'>Confirm and Transfer</button>
                     </div>
